e4c4ce7e069eaa9c605c16802176508a078e4788
Khroxx <134767175+Khroxx@users.noreply.github.com>
2024-08-17 19:07:12 +0200

    greetings responsive + orientation

--- videoflix/src/app/shared/header/header.component.ts
+++ videoflix/src/app/shared/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,10 +9,17 @@ import { Router } from '@angular/router';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  logoImg: string = '';
 
   constructor(
     private router: Router
-  ){}
+  ){
+    this.isWindowSizeSmall();
+  }
+
+  ngonInit(){
+    this.isWindowSizeSmall();
+  }
 
   navigateToLogin(){
     this.router.navigate(['welcome/login'])
@@ -20,6 +27,21 @@ export class HeaderComponent {
 
   get isLoginRoute(){
     return this.router.url.includes('login')
+  }
+
+  isWindowSizeSmall(){
+    if (typeof window !== 'undefined'){
+      if (window.innerWidth < 460 || window.innerWidth > window.innerHeight) {
+        this.logoImg = 'img/logo_only.png';
+      } else {
+      this.logoImg = 'img/logo.png';
+      }
+    }
+  }
+  
+  @HostListener('window:resize', ['$event'])
+  onResize(){
+    this.isWindowSizeSmall();
   }
 }
 
